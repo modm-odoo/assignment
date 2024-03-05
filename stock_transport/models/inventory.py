6f3579f63144ae630bde9d765d9a29d166d35014
modm-odoo <modm@odoo.com>
2024-03-05 14:24:15 +0530

    done

--- stock_transport/models/inventory.py
+++ stock_transport/models/inventory.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
-
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from odoo import models, Command, fields, api
 
 class InheritedModel2(models.Model):
@@ -10,10 +11,32 @@ class InheritedModel2(models.Model):
     vehicle_category_id = fields.Many2one('fleet.vehicle.model.category')
     weight = fields.Float(compute="_compute_weight_volume")
     volume = fields.Float(compute="_compute_weight_volume")
+    leave_id = fields.Many2one(
+        'resource.calendar.leaves',
+        help='Slot into workcenter calendar once planned',
+        check_company=True, copy=False)
+    date_start = fields.Datetime(
+        'Start',
+        compute='_compute_dates',
+        inverse='_set_dates',
+        store=True, copy=False)
+    date_finished = fields.Datetime(
+        'End',
+        compute='_compute_dates',
+        inverse='_set_dates',
+        store=True, copy=False)
+    state = fields.Selection([
+        ('pending', 'Waiting for another Vehicle'),
+        ('waiting', 'Waiting for vehicle'),
+        ('ready', 'Ready'),
+        ('progress', 'In Progress'),
+        ('done', 'Finished'),
+        ('cancel', 'Cancelled')], string='Status',
+        compute='_compute_state', store=True,
+        default='pending', copy=False, readonly=True, recursive=True, index=True)
 
     @api.depends("vehicle_category_id")
     def _compute_weight_volume(self):
-        self.ensure_one()
         move_line_ids=[]
 
         weight = 0
@@ -23,7 +46,7 @@ class InheritedModel2(models.Model):
             move_line_ids.append(move_line_id.id)
 
         move_lines = self.env["stock.move.line"].browse(move_line_ids)
-
+        # breakpoint()
         for move_line in move_lines:
             weight += move_line.product_id.weight * move_line.quantity
             volume += move_line.product_id.volume * move_line.quantity
@@ -37,3 +60,28 @@ class InheritedModel2(models.Model):
             self.volume = volume / self.vehicle_category_id.max_volume
         else:
             self.volume = 0
+
+    @api.depends('leave_id')
+    def _compute_dates(self):
+        for vehicle in self:
+            vehicle.date_start = vehicle.leave_id.date_from
+            vehicle.date_finished = vehicle.leave_id.date_to
+
+    def _set_dates(self):
+        for wo in self.sudo():
+            if wo.leave_id:
+                if (not wo.date_start or not wo.date_finished):
+                    raise UserError(_("It is not possible to unplan one single Work Order. "
+                              "You should unplan the Manufacturing Order instead in order to unplan all the linked operations."))
+                wo.leave_id.write({
+                    'date_from': wo.date_start,
+                    'date_to': wo.date_finished,
+                })
+            elif wo.date_start and wo.date_finished:
+                wo.leave_id = wo.env['resource.calendar.leaves'].create({
+                    'name': wo.display_name,
+                    'date_from': wo.date_start,
+                    'date_to': wo.date_finished,
+                    'resource_id': wo.vehicle.id,
+                    'time_type': 'other',
+                })
---
+++ stocktrans/__manifest__.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Stock Trans",
+    "version": "1.0",
+    "summary": "It is a stock transport application",
+    "installable": True,
+    "application": True,
+    "auto_install": True,
+    "license": "OEEL-1",
+    "depends": ["base", "stock", "fleet", "mail",], 
+    "data": [
+        "views/inherited_setting.xml",
+        ]
+}
