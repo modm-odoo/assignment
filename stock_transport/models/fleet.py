97a234f1fd372cddd8e21f62d7172930c422291a
modm-odoo <modm@odoo.com>
2024-03-06 10:25:47 +0530

    minor change

--- stock_transport/models/fleet.py
+++ stock_transport/models/fleet.py
@@ -6,7 +6,7 @@ class InheritedModel(models.Model):
     _inherit = ["fleet.vehicle.model.category"]
 
     max_weight = fields.Float(string="Max Weight(kg)", default=1)
-    max_volume = fields.Float(string="Max Volume(m3)", default=1)
+    max_volume = fields.Float(string="Max Volume(m\u00b3)", default=1)
 
     @api.depends('name', 'max_weight', 'max_volume')
     def _compute_display_name(self):
--- stock_transport/models/inventory.py
+++ stock_transport/models/inventory.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 from odoo import api, fields, models
-from datetime import timedelta
+from datetime import *
 
 class StockPickingBatch(models.Model):
     _inherit = "stock.picking.batch"
@@ -16,7 +16,7 @@ class StockPickingBatch(models.Model):
     moves_count = fields.Integer(string="Move Lines", compute="_compute_moves_number", store=True)
     transfers_count = fields.Integer(string="Transfer Lines", compute="_compute_picking_number", store=True)
 
-    @api.depends("vehicle_category_id")
+    @api.depends("vehicle_category_id","weight","volume")
     def _compute_weight_volume(self):
         for record in self:
             weight = 0
@@ -29,7 +29,7 @@ class StockPickingBatch(models.Model):
             record.weight = (weight / record.vehicle_category_id.max_weight) * 10 if record.vehicle_category_id.max_weight != 0 else 0
             record.volume = (volume / record.vehicle_category_id.max_volume) * 10 if record.vehicle_category_id.max_volume != 0 else 0
             
-    @api.depends("create_date", "scheduled_date")
+    @api.depends("scheduled_date")
     def _compute_dates(self):
         for record in self:
             start_date = record.scheduled_date
@@ -46,3 +46,8 @@ class StockPickingBatch(models.Model):
     def _compute_picking_number(self):
         for record in self:
             record.transfers_count = len(record.picking_ids)
+
+    @api.depends("vehicle_category_id")
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = f'{rec.name} : ({rec.vehicle_category_id.max_weight},{rec.vehicle_category_id.max_volume})'
